89958408bbe5f9a743cdb1e1eabf9d7186ff1e7c
Xavier <shottyx5156@hotmail.com>
2022-12-02 22:58:25 -0500

    adds event listener and functions

--- script.js
+++ script.js
@@ -9,8 +9,32 @@ const settings = {
 		"X-RapidAPI-Host": "mcu-comics-and-characters.p.rapidapi.com"
 	}
 };
+// const url = "https://mcu-comics-and-characters.p.rapidapi.com/mcu/characters"
+/////// Elements Refered
+const $name = $( '#name' )
+const $powers = $( '#powers' )
+const $input = $( 'input[type="text"]' )
+const $form = $( 'form' )
 
-$.ajax(settings).done(function(response) {
-	console.log(response);
-});
+// $.ajax(settings).done(function(response) {
+// 	console.log(response);
+// });
 
+/////// Event Listeners
+$form.on('submit', handleGetData)
+
+///////// Functions 
+
+function handleGetData(event) {
+    // event.preventDefault();
+    userInput = $input.val()
+    $.ajax( url+userInput ).then( (data ) => {
+        console.log(data)
+        $name.text(data.$name)
+        $powers.text(data.Powers)
+    }, (error) => {
+        console.log('bad request', error)
+        console.log('handleGetData');
+    })
+}
+/// handleGetData();
