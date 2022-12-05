6e85df525feae15cc6cf86013b9ce6bffbc50e20
Xavier <shottyx5156@hotmail.com>
2022-12-04 21:36:59 -0500

    updated listeners

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 
 const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://mcu-comics-and-characters.p.rapidapi.com/mcu/characters",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "b90f6f188emsh2aa3a1173653cb2p1db342jsn62cd4bffb33d",
-		"X-RapidAPI-Host": "mcu-comics-and-characters.p.rapidapi.com"
+	async: true,
+	crossDomain: true,
+	url: 'https://mcu-comics-and-characters.p.rapidapi.com/mcu/characters',
+	method: "GET",
+	headers: {
+		'X-RapidAPI-Key': "b90f6f188emsh2aa3a1173653cb2p1db342jsn62cd4bffb33d",
+		'X-RapidAPI-Host' : "mcu-comics-and-characters.p.rapidapi.com"
 	}
 };
+
+// const URL = "https://mcu-comics-and-characters.p.rapidapi.com/mcu/characters"
+
 // const url = "https://mcu-comics-and-characters.p.rapidapi.com/mcu/characters"
 /////// Elements Refered
-const $name = $( '#name' )
-const $powers = $( '#powers' )
-const $input = $( 'input[type="text"]' )
-const $form = $( 'form' )
+const $name = $('#name');
+const $powers = $('#powers');
+const $input = $('input[type="text"]');
+const $form = $('form');
 
 // $.ajax(settings).done(function(response) {
 // 	console.log(response);
 // });
 
 /////// Event Listeners
-$form.on('submit', handleGetData)
+$name.on("submit", handleGetData);
+$powers.on("submit", handleGetData);
+$input.on("submit", handleGetData);
+$form.on("submit", handleGetData);
+
+
 
 ///////// Functions 
 
 function handleGetData(event) {
-    // event.preventDefault();
+     //event.preventDefault();
     userInput = $input.val()
-    $.ajax( url+userInput ).then( (data ) => {
-        console.log(data)
-        $name.text(data.$name)
+    
+    $.ajax(settings+userInput).then( ( data ) => {
+        console.log( data )
+        $name.text(data.$Name)
         $powers.text(data.Powers)
-    }, (error) => {
+    }, ( error ) => {
         console.log('bad request', error)
-        console.log('handleGetData');
+        
     })
 }
 /// handleGetData();
