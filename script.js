a8d355365291ae0291f5d7f783d9336004352c81
XAVDAVIS <117596121+XAVDAVIS@users.noreply.github.com>
2022-12-11 21:37:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 
 const URL = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json";
-
-///// CONSTANTS //////
 const $name = $( '#name' )
+
+///// POWERSTATS CONSTANTS //////
+
 const $intelligence = $( '#intelligence' )
 const $appearance = $( '#appearance' )
 const $speed = $( '#speed' )
-const $durability =( '#durability' )
+const $durability = $( '#durability' )
 const $power = $( '#power' )
+const $combat = $( '#combat' )
+const $strength = $( '#strength' )
+
+///// BIOGRAPHY CONSTANTS /////
+
+const $fullname = $( '#full-name' )
+const $alteregos = $( '#alter-egos' )
+const $aliases = $( '#aliases' )
+const $placeofbirth = $( '#place-of-birth' )
+
 const $work = $( '#work' )
 const $connections = $( '#connections' )
 const $form = $('form') 
@@ -31,7 +42,15 @@ function handleGetData(event) {
     $intelligence.text(result.powerstats.intelligence);
     $speed.text(result.powerstats.speed);
     $power.text(result.powerstats.power);
-    $('').append(`<img src="${heros.images}" alt="${heros.name}"/>`);
-    // $durability.text(result.powerstats.durability);
+    $durability.text(result.powerstats.durability);
+    $combat.text(result.powerstats.combat);
+    $strength.text(result.powerstats.strength);
+    
+    $fullname.text(result.biography.fullName);
+    $alteregos.text(result.biography.alterEgos);
+    $aliases.text(result.biography.aliases);
+    $placeofbirth.text(result.biography.placeOfBirth);
+
     console.log(result);
+    // document.querySelector('.app-body-content-thumbnail').innerHTML = `<img src ="${heros.image.URL}">`;
 }
